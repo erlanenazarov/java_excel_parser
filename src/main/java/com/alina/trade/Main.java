a97f3f017967775dcde011c42aecc1b3e0bbd36c
erlanenazarov <admin@abovethesky.ru>
2017-01-22 01:23:01 +0600

    + Initial commit

---
+++ src/main/java/com/alina/trade/Main.java
@@ -0,0 +1,10 @@
+package com.alina.trade;
+
+import com.alina.trade.Parser;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Parser.parse("testfile.xls"));
+    }
+}
---
+++ src/main/java/com/alina/trade/Parser.java
@@ -0,0 +1,60 @@
+package com.alina.trade;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import sun.org.mozilla.javascript.json.JsonParser;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+
+public class Parser {
+
+    public static String parse(String path) {
+        String result = "";
+        InputStream in;
+        HSSFWorkbook wb = null;
+        try {
+            in = new FileInputStream(path);
+            wb = new HSSFWorkbook(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assert wb != null;
+        Sheet sheet = wb.getSheetAt(0);
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                int cellType = cell.getCellType();
+                switch (cellType) {
+                    case Cell.CELL_TYPE_STRING:
+                        result += cell.getStringCellValue() + "=";
+                        break;
+                    case Cell.CELL_TYPE_NUMERIC:
+                        result += "[" + cell.getNumericCellValue() + "]";
+                        break;
+
+                    case Cell.CELL_TYPE_FORMULA:
+                        result += "[" + cell.getNumericCellValue() + "]";
+                        break;
+                    default:
+                        result += "|";
+                        break;
+                }
+            }
+            result += "\n";
+        }
+
+        return result;
+    }
+
+    public static String getData(String data) {
+
+
+        return null;
+    }
+}
